1e92b641a48b736de4aac3ad0646ed9f22b725c3
JAEE <jaeework@gmail.com>
2023-08-05 04:14:28 +0900

    Programmers Java lv.0 주사위 게임02

--- lv.0/dice02.java
+++ lv.0/dice02.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -13,9 +15,16 @@ public class dice02 {
 
     private static int solution(int a, int b, int c) {
 
-        
+        Set<Integer> diceSet = new HashSet<Integer>(Arrays.asList(a, b, c));
 
-        return 0;
+        double answer = diceSet.size() == 3 ? a + b + c : 
+                        (diceSet.size() == 2 ? (a + b + c) * (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2)) : 
+                        (a + b + c) * (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2)) * (Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3)));
+
+
+        return (int)answer;
     }
+
+    // a == b && b == c, a == b || a ==c || b ==c 이용해서 풀어보기
     
 }
