5c0dde6b8b5a007019b7268300eaa1b823288774
JAEE <jaeework@gmail.com>
2023-08-06 05:23:26 +0900

    programmers java lv.0_이어 붙인 수
    programmers java lv.0_이어 붙인 수

---
+++ lv.0/linkedNum.java
@@ -0,0 +1,31 @@
+/**
+ * 이어 붙인 수
+ */
+public class linkedNum {
+    
+    public static void main(String[] args) {
+        int[] num_list = new int[]{3, 4, 5, 2, 1};
+        int result = solution(num_list);
+
+        System.out.println(result);
+    }
+
+    private static int solution(int[] num_list) {
+
+        String evenString = "";
+        String oddString = "";
+
+        for(int num : num_list) {
+            if(num % 2 == 0) {
+                evenString += "" + num;
+            } else {
+                oddString += "" + num;
+            }
+        }
+
+        int even = Integer.valueOf(evenString);
+        int odd = Integer.valueOf(oddString);
+
+        return even + odd;
+    }
+}
