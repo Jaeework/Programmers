1e0f1657a9ad3369ce90524c935e5e7bd3a0de0a
JAEE <jaeework@gmail.com>
2023-08-07 20:44:06 +0900

    programmers java lv.0_간단한 논리 연산
    programmers java lv.0_간단한 논리 연산

---
+++ lv.0/booleanOp.java
@@ -0,0 +1,18 @@
+/**
+ * 간단한 논리 연산
+ */
+public class booleanOp {
+    
+    public static void main(String[] args) {
+        boolean result = solution(true, false, false, false);
+
+        System.out.println(result);
+    }
+
+    private static boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
+        boolean answer = (x1 || x2) && (x3 || x4);
+              
+        return answer;
+    }
+
+}
