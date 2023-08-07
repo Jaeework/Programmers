f92014f9ef0f66ca1ece077fcfee02a7a99a8cc0
JAEE <jaeework@gmail.com>
2023-08-07 20:43:37 +0900

    programmers java lv.0_카운트 업
    programmers java lv.0_카운트 업

---
+++ lv.0/countUp.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+/**
+ * 카운트 업
+ */
+public class countUp {
+    
+    public static void main(String[] args) {
+        int[] result = solution(3, 10);
+
+        System.out.println(Arrays.toString(result));
+    }
+
+    private static int[] solution(int start, int end) {
+        int[] answer = new int[end - start + 1];
+
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = start + i;
+        }
+
+        return answer;
+    }
+}
