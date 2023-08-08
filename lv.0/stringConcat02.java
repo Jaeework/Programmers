bf40a7e9d918619b3179bcc61fb3ff2835a4ee79
JAEE <jaeework@gmail.com>
2023-08-08 11:09:21 +0900

    programmers java lv.0_글자 이어 붙여 문자열 만들기
    programmers java lv.0_글자 이어 붙여 문자열 만들기

---
+++ lv.0/stringConcat02.java
@@ -0,0 +1,22 @@
+/**
+ * 글자 이어 붙여 문자열 만들기
+ */
+public class stringConcat02 {
+    
+    public static void main(String[] args) {
+        int[] index_list = {16, 6, 5, 3, 12, 14, 11, 11, 17, 12, 7};
+        String result = solution("cvsgiorszzzmrpaqpe", index_list);
+
+        System.out.println(result);
+    }
+
+    private static String solution(String my_string, int[] index_list) {
+        String answer = "";
+
+        for(int idx : index_list) {
+            answer += my_string.charAt(idx);
+        }
+
+        return answer;
+    }
+}
