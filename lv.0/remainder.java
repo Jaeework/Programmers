1bf261c07e438c265681b1abbac67be2b608e2fb
JAEE <jaeework@gmail.com>
2023-08-08 11:09:40 +0900

    programmers java lv.0_9로 나눈 나머지
    programmers java lv.0_9로 나눈 나머지

---
+++ lv.0/remainder.java
@@ -0,0 +1,22 @@
+/**
+ * 9로 나눈 나머지
+ */
+public class remainder {
+    
+    public static void main(String[] args) {
+        int result = solution("78720646226947352489");
+
+        System.out.println(result);
+    }
+
+    private static int solution(String number) {
+        int sum = 0;
+
+        for(char ch : number.toCharArray()) {
+            sum += Character.getNumericValue(ch);
+        }
+        int answer = sum % 9;
+
+        return answer;
+    }
+}
