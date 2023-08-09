48a76f613b3e00fc88e8ce9efec964e6cc1108e9
JAEE <jaeework@gmail.com>
2023-08-09 23:37:22 +0900

    programmers java lv.0_몫 구하기
    programmers java lv.0_몫 구하기

---
+++ lv.0/quotient.java
@@ -0,0 +1,15 @@
+/**
+ * 몫 구하기
+ */
+public class quotient {
+
+    public static void main(String[] args) {
+        int result = solution(10, 5);
+        System.out.println(result);
+    }
+
+    private static int solution(int num1, int num2) {
+        return num1 / num2;
+    }
+    
+}
