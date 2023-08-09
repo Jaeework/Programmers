370d1449613a3a23cc358cd0e0c3eb03fb2996cb
JAEE <jaeework@gmail.com>
2023-08-09 23:37:04 +0900

    programmers java lv.0_숫자 비교하기
    programmers java lv.0_숫자 비교하기

---
+++ lv.0/compareNums02.java
@@ -0,0 +1,17 @@
+/**
+ * 숫자 비교하기
+ */
+public class compareNums02 {
+    
+    public static void main(String[] args) {
+        int result = solution(2, 3);
+        System.out.println(result);    
+    }
+
+    private static int solution(int num1, int num2) {
+        
+        return (num1 == num2) ? 1 : -1;
+    }
+
+
+}
