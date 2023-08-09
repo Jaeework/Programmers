62d5cf3ca0252786c5b257452749f25f6c8c862f
JAEE <jaeework@gmail.com>
2023-08-09 23:37:49 +0900

    programmers java lv.0_두 수의 곱
    programmers java lv.0_두 수의 곱

---
+++ lv.0/multiply.java
@@ -0,0 +1,14 @@
+/**
+ * 두 수의 곱
+ */
+public class multiply {
+    
+    public static void main(String[] args) {
+        int result = solution(3, 4);
+        System.out.println(result);
+    }
+
+    private static int solution(int num1, int num2) {
+        return num1 * num2;
+    }
+}
