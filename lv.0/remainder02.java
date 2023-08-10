c1308e58c819308002e6b91d661f937dcea55404
JAEE <jaeework@gmail.com>
2023-08-10 22:59:53 +0900

    programmers java lv.0_나머지 구하기
    programmers java lv.0_나머지 구하기

---
+++ lv.0/remainder02.java
@@ -0,0 +1,15 @@
+/**
+ * 나머지 구하기
+ */
+public class remainder02 {
+    
+    public static void main(String[] args) {
+        int result = solution(3, 2);
+
+        System.out.println(result);
+    }
+
+    private static int solution(int num1, int num2) {
+        return num1 % num2;
+    }
+}
