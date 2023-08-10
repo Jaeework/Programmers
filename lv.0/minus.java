0fc7356b103abe690eb6ff6b177d8d05563b477a
JAEE <jaeework@gmail.com>
2023-08-10 23:04:21 +0900

    programmers java lv.0_두 수의 차
    programmers java lv.0_두 수의 차

---
+++ lv.0/minus.java
@@ -0,0 +1,15 @@
+/**
+ * 두 수의 차
+ */
+public class minus {
+    
+    public static void main(String[] args) {
+        int result = solution(2, 3);
+
+        System.out.println(result);
+    }
+
+    private static int solution(int num1, int num2) {
+        return num1 - num2;
+    }
+}
