cf6b1641cfcfb1940019f7f6969b1840e9d62ba4
JAEE <jaeework@gmail.com>
2023-08-10 23:37:19 +0900

    programmers java lv.0_두 수의 나눗셈
    programmers java lv.0_두 수의 나눗셈

---
+++ lv.0/division.java
@@ -0,0 +1,15 @@
+/**
+ * 두 수의 나눗셈
+ */
+public class division {
+    
+    public static void main(String[] args) {
+        int result = solution(7, 3);
+
+        System.out.println(result);
+    }
+
+    private static int solution(int num1, int num2) {
+        return (int)(num1 / (double)num2 * 1000);
+    }
+}
