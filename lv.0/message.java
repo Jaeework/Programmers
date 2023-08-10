a64789a0aba20742b7aef2b2d57f81856c0ed42b
JAEE <jaeework@gmail.com>
2023-08-11 01:08:58 +0900

    programmers java lv.0_편지
    programmers java lv.0_편지

---
+++ lv.0/message.java
@@ -0,0 +1,17 @@
+/**
+ * 편지
+ */
+public class message {
+    
+    public static void main(String[] args) {
+        String message = "happy birthday!";
+        int result = solution(message);
+
+        System.out.println(result);
+    }
+
+    private static int solution(String message) {
+        
+        return message.length() * 2;
+    }
+}
