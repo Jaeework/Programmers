a55ad04cbd55410d03fe7931aca3a530ca7a2fb0
JAEE <jaeework@gmail.com>
2023-08-11 23:05:42 +0900

    programmers java lv.0_최댓값 만들기 1
    programmers java lv.0_최댓값 만들기 1

---
+++ lv.0/makingMax.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+/**
+ * 최댓값 만들기 (1)
+ */
+public class makingMax {
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        int result = solution(numbers);
+
+        System.out.println(result);
+    }
+
+    private static int solution(int[] numbers) {
+        Arrays.sort(numbers);
+        
+        return numbers[numbers.length - 1] * numbers[numbers.length - 2];
+    }
+    
+}
