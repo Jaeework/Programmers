f1218eda3c901e69d9631cbc50d2b17b0810bd24
JAEE <jaeework@gmail.com>
2023-08-12 23:55:11 +0900

    programmers java lv.0_짝수 홀수 개수
    programmers java lv.0_짝수 홀수 개수

---
+++ lv.0/evenOdd03.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+/**
+ * 짝수 홀수 개수
+ */
+public class evenOdd03 {
+
+    public static void main(String[] args) {
+        int[] num_list = {1, 2, 3, 4, 5};
+        int[] result = solution(num_list);
+
+        System.out.println(Arrays.toString(result));
+    }
+
+    private static int[] solution(int[] num_list) {
+        int evenCnt = 0;
+        int oddCnt = 0;
+
+        for(int num : num_list) {
+            if(num % 2 == 0) {evenCnt++;}
+            else {oddCnt++;}
+        }
+
+
+        return new int[]{evenCnt, oddCnt};
+    }
+    
+}
