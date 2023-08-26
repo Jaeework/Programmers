9593f49525892f9a0437feb2c4696bdfa05348e5
JAEE <jaeework@gmail.com>
2023-08-27 06:47:47 +0900

    programmers java lv.0_n개 간격의 원소들
    programmers java lv.0_n개 간격의 원소들

--- lv.0/Test89.java
+++ lv.0/Test89.java
@@ -15,7 +15,6 @@ public class Test89 {
 
     private static int[] solution(int[] num_list, int n) {
         int[] answer = new int[(num_list.length % n == 0) ? num_list.length / n : num_list.length / n + 1];
-        System.out.println(answer.length);
 
         for(int i = 0; i < answer.length; i++) {
             answer[i] = num_list[n * i];
