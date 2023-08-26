c4b29c4e0c20c44e9a5d8bed94ad6a41634e5b54
JAEE <jaeework@gmail.com>
2023-08-27 07:46:20 +0900

    programmers java lv.0_정수 부분
    programmers java lv.0_정수 부분

---
+++ lv.0/Test97.java
@@ -0,0 +1,17 @@
+/**
+ * 정수 부분
+ */
+public class Test97 {
+
+    public static void main(String[] args) {
+        double flo = 1.42;
+
+        int result = solution(flo);
+        System.out.println(result);
+    }
+
+    private static int solution(double flo) {
+        return (int)flo;
+    }
+    
+}
