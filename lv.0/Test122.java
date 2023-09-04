bf183b7a2fd761159e18bd3aea457d671f80be2a
JAEE <jaeework@gmail.com>
2023-09-04 19:23:11 +0900

    programmers java lv.0_뒤에서 5등까지
    programmers java lv.0_뒤에서 5등까지

---
+++ lv.0/Test122.java
@@ -0,0 +1,27 @@
+/*
+ * 뒤에서 5등까지
+ */
+
+import java.util.Arrays;
+
+public class Test122 {
+    
+    public static void main(String[] args) {
+        int[] num_list = {12, 4, 15, 46, 38, 1, 14};
+
+        int[] result = solution(num_list);
+        System.out.println(Arrays.toString(result));
+    }
+
+    private static int[] solution(int[] num_list) {
+        int[] answer = new int[5];
+
+        Arrays.sort(num_list);
+
+        for(int i = 0 ; i < 5; i++) {
+            answer[i] = num_list[i];
+        }
+
+        return answer;
+    }
+}
