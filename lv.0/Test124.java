1d62b8138b03b291984a2adaf1dd16b90749b7ec
JAEE <jaeework@gmail.com>
2023-09-05 22:24:44 +0900

    programmers java lv.0_n의 배수 고르기
    programmers java lv.0_n의 배수 고르기

---
+++ lv.0/Test124.java
@@ -0,0 +1,38 @@
+/*
+ * n의 배수 고르기
+ */
+
+import java.util.Arrays;
+
+public class Test124 {
+    
+    public static void main(String[] args) {
+        int n = 12;
+        int[] numlist = {2, 100, 120, 600, 12, 12};
+
+        int[] result = solution(n, numlist);
+        System.out.println(Arrays.toString(result));
+    }
+
+    private static int[] solution(int n, int[] numlist) {
+        int[] answer = {};
+
+        int cnt = 0;
+
+        for(int i = 0; i < numlist.length; i++) {
+            if(numlist[i] % n == 0) {
+                cnt++;
+            }
+        }
+        System.out.println("cnt : " + cnt);
+        answer = new int[cnt];
+
+        int idx = 0;
+
+        for(int num : numlist) {
+            if(num % n == 0) {answer[idx++] = num;}
+        }
+
+        return answer;
+    }
+}
